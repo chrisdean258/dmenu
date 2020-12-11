a10bd08552eca97e0f8d96b43ba3f71079591d5d
Chris Dean <chrisdean258@gmail.com>
2020-12-11 02:48:52 -0500

    dmenu_run history script

--- config.h
+++ config.h
@@ -2,18 +2,16 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+static int centered = 0;                    /* -c option; centers dmenu on screen */
+static int min_width = 960;                    /* minimum width when centered */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-#ifndef HD
-	"monospace:size=10"
-#else
 	"monospace:size=16"
-#endif
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeNorm] = { "#ffffff", "#121212" },
 	[SchemeSel] = { "#eeeeee", "#005577" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
